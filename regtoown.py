099b2d8a8c3a735e726d8db10dac63d6b4b9dc85
Artyom Solovyev <artema16-01@mail.ru>
2024-07-03 10:29:21 +0400

    Выборы президента

--- regtoown.py
+++ regtoown.py
@@ -14,10 +14,25 @@ for i in range(len(prov)):
         n = s.find("controller =")
         m = s.find("\n",n)
         s = s[:n] + "controller = " + country + s[m:]
-        
         n = s.find("add_core =")
         m = s.find("\n",n)
         s = s[:n] + "add_core = " + country + s[m:]
+
+        scan = s.find("1861")
+        if scan:
+            n = s.find("owner =",scan)
+            m = s.find("\n",n)
+            s = s[:n] + "owner = " + country + s[m:]
+            n = s.find("controller =",scan)
+            m = s.find("\n",n)
+            s = s[:n] + "controller = " + country + s[m:]
+            n = s.find("add_core =",scan)
+            m = s.find("\n",n)
+            s = s[:n] + "add_core = " + country + s[m:]
+
+
+
+                
         n = s.rfind("}")
         s = s[:n+1]
     with open(files[0],'w') as f: 
