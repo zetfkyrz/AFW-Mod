01be8ca8fda611f452732412155dfe2cf07f39fe
Artyom Solovyev <artema16-01@mail.ru>
2024-07-29 17:37:53 +0400

    Fixed Changecontroller script

--- changecontrollerPlusPlus.py
+++ changecontrollerPlusPlus.py
@@ -1,4 +1,5 @@
 import glob
+
 country = input()
 
 prov = list(map(int,input().split()))
@@ -9,27 +10,11 @@ for i in range(len(prov)):
     with open(files[0],'r' ) as f:
         s = f.read()
         scan = s.find("1861")
-        if scan>0:
-            n = s.find("owner =",scan)
-            if n>0:
+        end = s.find('}',scan)
+        n = s.find("owner =",scan,end)
+        if scan > 0 and n > 0:
                 m = s.find("\n",n)
                 s = s[:n] + "owner = " + country + "\n" + s[m:]
-                n = s.find("controller =",scan)
-                if n>0:
-                    m = s.find("\n",n)
-                    s = s[:n] + "controller = " + country + "\n" + s[m:]
-                    n = s.rfind("}")
-                    s = s[:n+1]
-                else:
-                    n = s.find("owner =",scan)
-                    m = s.find("\n",n)
-                    s = s[:m+1] + "controller = " + country + "\n" + s[m:]
-                    n = s.rfind("}")
-                    s = s[:n+1]
-            else:
-                n = s.find("1861.1.1 = {")
-                m = s.find("\n",n)
-                s = s[:m+1] + "owner = " + country + "\n" + s[m+1:]
                 n = s.find("controller =",scan)
                 if n>0:
                     m = s.find("\n",n)
@@ -49,7 +34,7 @@ for i in range(len(prov)):
             n = s.find("controller =")
             if n>0:
                  m = s.find("\n",n)
-                 s = s[:n] + "controller = " + country + "\n" + s[m:]
+                 s = s[:n] + "controller = " + country + "\n" + s[m+1:]
                  n = s.rfind("}")
                  s = s[:n+1]
             else:
